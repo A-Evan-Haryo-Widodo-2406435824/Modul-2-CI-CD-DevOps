af35ae0c854ff61b9de829279b346483962ceac7
evanhwz <evan.haryo@ui.ac.id>
2026-02-22 22:54:25 +0700

    fix: fix sonar on uncovered test

--- build.gradle.kts
+++ build.gradle.kts
@@ -85,6 +85,11 @@ tasks.test{
 
 tasks.jacocoTestReport{
     dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+        html.required.set(true)
+    }
 }
 
 tasks.withType<Test>().configureEach {
