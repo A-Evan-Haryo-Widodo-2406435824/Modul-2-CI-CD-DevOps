eaf0cc1b84645c9c0b43554c6790ec87e9c87b7c
evanhwz <evan.haryo@ui.ac.id>
2026-02-22 12:12:03 +0700

    test: add controller testcase

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomePageControllerTest.java
@@ -0,0 +1,22 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class HomePageControllerTest {
+
+    @InjectMocks
+    private HomePageController homePageController;
+
+
+    @Test
+    void testHomePage(){
+        String viewName = homePageController.Homepage();
+        assertEquals("HomePage", viewName);
+    }
+
+}
