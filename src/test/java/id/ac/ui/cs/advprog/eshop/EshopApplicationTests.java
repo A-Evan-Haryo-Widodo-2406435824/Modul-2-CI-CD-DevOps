2952cdc24f7b2ad4b45baf7f99e0866b73c7b086
evanhwz <evan.haryo@ui.ac.id>
2026-02-22 15:19:19 +0700

    test: add test of main func so it reaches 100% coverage

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -2,12 +2,21 @@ package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class EshopApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void main(){
+        String[] args = {"--spring.main.web-application-type=none"};
+        assertDoesNotThrow(() -> {
+            EshopApplication.main(args);
+        });
     }
 
 }
